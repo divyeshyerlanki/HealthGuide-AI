b8fec865b378674a1785825ea003af67930eeaed
Divyesh Yerlanki <divyeshyerlanaki@gmail.com>
2026-09-11 12:29:27 +0530

    Configure Aiven MySQL SSL

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ const db = mysql.createPool({
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
